568c4ad0bb9f3bacd4e4e0fbeb22708f3042db70
otsamzy <109823511+otsamzy@users.noreply.github.com>
2024-02-08 08:22:15 +0100

    Db.js

--- DB/Db.js
+++ DB/Db.js
@@ -1,7 +1,12 @@
 const mongoose = require("mongoose");
 
 const connectionDb = (url) => {
-  return mongoose.connect(url);
+return mongoose.connect(url, {
+
+  useNewUrlParser: "true",
+  useUnifiedTopology: "true"
+
+});
 };
 
 module.exports = connectionDb;
