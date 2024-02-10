ccbd431a478ce0c889fdc404b910dfddf860cad0
otsamzy <109823511+otsamzy@users.noreply.github.com>
2024-02-10 22:18:37 +0100

    app.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 const port =process.env.PORT || 3000;
 app.use(express.static("./Frontend"));
 app.use(express.json());
+app.use(cors());
 app.use("/api/v1/tasks", tasks);
 
 const start = async () => {
