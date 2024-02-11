3b663eef218a788179ac05c6a86d65c0d5df359f
otsamzy <109823511+otsamzy@users.noreply.github.com>
2024-02-11 09:22:25 +0100

    app.js

--- app.js
+++ app.js
@@ -4,14 +4,9 @@ require("dotenv").config();
 const tasks = require("./Routes/router");
 const cors = require("cors");
 const app = express();
-<<<<<<< HEAD
 app.use(cors());
 const port = process.env.PORT || 3000;
-
-=======
-const port =process.env.PORT || 3000;
 app.use(express.static("./Frontend"));
->>>>>>> ccbd431a478ce0c889fdc404b910dfddf860cad0
 app.use(express.json());
 app.use(cors());
 app.use("/api/v1/tasks", tasks);
