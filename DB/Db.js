14f25a8824a12a2b70b5fe2d2e435860496ae319
Tochukwu Odu <otsamzy67@gmail.com>
2024-02-11 07:25:05 +0100

    i changed some committed

--- DB/Db.js
+++ DB/Db.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
 
-const connectionDb = (url) => {
-  return mongoose.connect(url);
+const connectionDb = async () => {
+  try {
+    const connect = await mongoose.connect(process.env.CONNT_STRG);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = connectionDb;
